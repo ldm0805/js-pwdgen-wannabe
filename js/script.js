68ed5de2953c4b0f086c941cccccc739faddd3f8
ldm0805 <leonardodimaggio94@gmail.com>
2022-11-24 15:34:41 +0100

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// Richiedere all'utente di inserire il suo nome; 
+let nome = prompt('Inserisci Il tuo nome');
+
+// Richiedere all'utente di inserire il suo cognome; 
+let cognome = prompt('Inserisci il tuo cognome');
+
+// Richiedere all'utente di inserire il suo colore preferito; 
+let color = prompt('Inserisci il tuo colore preferito');
+
+// 1.4 Inserire da script il numero 21;; 
+const tewnty_one = '21';
+
+//Restituire a schermo la password.
+document.getElementById('weak_pass').innerHTML = 'La tua password è' + " " + nome + cognome + color + tewnty_one;
