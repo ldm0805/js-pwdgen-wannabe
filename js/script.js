9f0523ab4bfb08a6f785d424a72123d6fa715e74
ldm0805 <leonardodimaggio94@gmail.com>
2022-11-25 09:45:19 +0100

    esercizio corretto

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let result = cognome.replace(" ", "_");
 let color = prompt('Inserisci il tuo colore preferito');
 
 // 1.4 Inserire da script il numero 21;; 
-const tewnty_one = '21';
+const twenty_one = 21;
 
-//Restituire a schermo la password.
-document.getElementById('weak_pass').innerHTML = 'La tua password è:' + " " + nome + result + color + tewnty_one;
+// Restituire a schermo la password.
+document.getElementById('weak_pass').innerHTML = 'La tua password è:' + " " + nome + result + color + twenty_one;
